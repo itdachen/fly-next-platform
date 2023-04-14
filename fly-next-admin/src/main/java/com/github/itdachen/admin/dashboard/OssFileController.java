0a1b816a09c9bbe24fe4aff7cceeb0c8bbf79999
王大宸 <240216240@qq.com>
2023-04-15 00:21:17 +0800

    添加文件上传

---
+++ fly-next-admin/src/main/java/com/github/itdachen/admin/dashboard/OssFileController.java
@@ -0,0 +1,112 @@
+package com.github.itdachen.admin.dashboard;
+
+import com.github.itdachen.admin.mapper.INetDiskMd5Mapper;
+import com.github.itdachen.admin.sdk.dto.NetDiskMd5Dto;
+import com.github.itdachen.admin.sdk.vo.NetDiskMd5Vo;
+import com.github.itdachen.admin.service.INetDiskMd5Service;
+import com.github.itdachen.framework.context.exception.BizException;
+import com.github.itdachen.framework.core.response.ServerResponse;
+import com.github.itdachen.framework.core.utils.StringUtils;
+import com.github.itdachen.framework.file.FileHelper;
+import com.github.itdachen.framework.file.entity.FileInfo;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description: 文件上传
+ * Created by 王大宸 on 2023/04/14 22:52
+ * Created with IntelliJ IDEA.
+ */
+@RestController
+@RequestMapping("/oss/file")
+public class OssFileController {
+
+    private final FileHelper fileHelper;
+    private final INetDiskMd5Service netDiskMd5Service;
+
+    public OssFileController(FileHelper fileHelper,
+                             INetDiskMd5Service netDiskMd5Service) {
+        this.fileHelper = fileHelper;
+        this.netDiskMd5Service = netDiskMd5Service;
+    }
+
+
+    /**
+     * 上传文件
+     */
+    @PostMapping("/upload")
+    public ServerResponse<FileInfo> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        if (file.isEmpty()) {
+            return ServerResponse.errMsg("上传文件不能为空");
+        }
+        final String md5Hex = fileHelper.md5Hex(file);
+        final NetDiskMd5Vo netDiskMd5 = netDiskMd5Service.findNetDiskMd5(md5Hex);
+        if (null == netDiskMd5) {
+            FileInfo upload = fileHelper.upload(file);
+            upload.setMd5(md5Hex);
+            netDiskMd5Service.saveNetDiskMd5(NetDiskMd5Dto.builder()
+                    .fileFormat(upload.getFormat())
+                    .fileSize(upload.getSize())
+                    .url(upload.getUrl())
+                    .md5(md5Hex)
+                    .fileName(upload.getName())
+                    .build());
+            return ServerResponse.okData(upload);
+        }
+        return ServerResponse.okData(new FileInfo.Builder()
+                .md5(md5Hex)
+                .url(netDiskMd5.getUrl())
+                .format(netDiskMd5.getFileFormat())
+                .size(netDiskMd5.getFileSize())
+                .name(netDiskMd5.getFileName())
+                .build());
+    }
+
+    /***
+     * 文件下载, 支持本地文件和网络文件
+     *
+     * @author 王大宸
+     * @date 2022/8/16 10:51
+     * @param response response
+     * @param uri 文件地址, http 请求地址
+     * @param filename 文件名称,如果不传,默认以文件地址名称
+     * @return void
+     */
+    @RequestMapping(value = "/download")
+    public void download(HttpServletResponse response,
+                         String uri,
+                         String filename) throws Exception {
+        if (StringUtils.isEmpty(uri)) {
+            throw new BizException("下载的文件地址不能为空");
+        }
+        fileHelper.download(response, uri, filename);
+    }
+
+    /***
+     * 文件 md5 检查
+     *
+     * @author 王大宸
+     * @date 2022/9/25 21:30
+     * @param md5Hex md5Hex
+     * @return com.itdachen.framework.core.response.ServerResponse<com.itdachen.oss.sdk.vo.FileNode>
+     */
+    @GetMapping("/md5/{md5Hex}")
+    @ResponseBody
+    public ServerResponse<FileInfo> checkUpMd5Hex(@PathVariable("md5Hex") String md5Hex) throws Exception {
+        NetDiskMd5Vo netDiskMd5 = netDiskMd5Service.findNetDiskMd5(md5Hex);
+        if (null != netDiskMd5) {
+            return ServerResponse.ok();
+        }
+        return ServerResponse.okData(new FileInfo.Builder()
+                .md5(md5Hex)
+                .url(netDiskMd5.getUrl())
+                .format(netDiskMd5.getFileFormat())
+                .size(netDiskMd5.getFileSize())
+                .name(netDiskMd5.getFileName())
+                .build());
+    }
+
+
+}
---
+++ fly-next-server/src/main/java/com/github/itdachen/admin/service/impl/NetDiskMd5ServiceImpl.java
@@ -0,0 +1,129 @@
+package com.github.itdachen.admin.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.itdachen.admin.entity.NetDiskMd5;
+import com.github.itdachen.admin.mapper.INetDiskMd5Mapper;
+import com.github.itdachen.admin.service.INetDiskMd5Service;
+import com.github.itdachen.admin.sdk.dto.NetDiskMd5Dto;
+import com.github.itdachen.admin.sdk.query.NetDiskMd5Query;
+import com.github.itdachen.admin.sdk.vo.NetDiskMd5Vo;
+import com.github.itdachen.framework.core.response.TableData;
+import com.github.itdachen.framework.webmvc.entity.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 文件MD5 serverImpl
+ *
+ * @author 王大宸
+ * @date 2023-04-14 23:20:54
+ */
+@Service
+public class NetDiskMd5ServiceImpl implements INetDiskMd5Service {
+    private static final Logger logger = LoggerFactory.getLogger(NetDiskMd5ServiceImpl.class);
+
+    private final INetDiskMd5Mapper netDiskMd5Mapper;
+
+    public NetDiskMd5ServiceImpl(INetDiskMd5Mapper netDiskMd5Mapper) {
+        this.netDiskMd5Mapper = netDiskMd5Mapper;
+    }
+
+    /***
+     * 分页
+     *
+     * @author 王大宸
+     * @date 2023-04-14 23:20:54
+     * @param params com.github.itdachen.admin.sdk.query.NetDiskMd5Query
+     * @return cn.edu.hubu.common.core.response.TableData<com.github.itdachen.admin.sdk.vo.NetDiskMd5Vo>
+     */
+    @Override
+    public TableData<NetDiskMd5Vo> findNetDiskMd5Page(NetDiskMd5Query params) throws Exception {
+        Page<NetDiskMd5Vo> page = PageHelper.startPage(params.getPage(), params.getLimit());
+        List<NetDiskMd5Vo> list = netDiskMd5Mapper.findNetDiskMd5Page(params);
+        return new TableData<NetDiskMd5Vo>(page.getTotal(), list);
+    }
+
+    /***
+     * 根据 文件MD5 查询文件信息
+     *
+     * @author 王大宸
+     * @date 2023/4/14 23:27
+     * @param md5Hex md5Hex
+     * @return com.github.itdachen.admin.sdk.vo.NetDiskMd5Vo
+     */
+    @Override
+    public NetDiskMd5Vo findNetDiskMd5(String md5Hex) {
+        return netDiskMd5Mapper.findNetDiskMd5(md5Hex);
+    }
+
+    /***
+     * 新增
+     *
+     * @author 王大宸
+     * @date 2023-04-14 23:20:54
+     * @param netDiskMd5Dto netDiskMd5Dto
+     * @return com.github.itdachen.admin.sdk.dto.netDiskMd5
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public NetDiskMd5Dto saveNetDiskMd5(NetDiskMd5Dto netDiskMd5Dto) throws Exception {
+        NetDiskMd5 netDiskMd5 = new NetDiskMd5();
+        BeanUtils.copyProperties(netDiskMd5Dto, netDiskMd5);
+        EntityUtils.setCreatAndUpdateInfo(netDiskMd5);
+        netDiskMd5Mapper.saveNetDiskMd5(netDiskMd5);
+        netDiskMd5Dto.setId(netDiskMd5.getId());
+        return netDiskMd5Dto;
+    }
+
+    /***
+     * 根据id查询
+     *
+     * @author 王大宸
+     * @date 2023-04-14 23:20:54
+     * @param id 需要查询数据的id
+     * @return com.github.itdachen.admin.sdk.vo.NetDiskMd5Vo
+     */
+    @Override
+    public NetDiskMd5Vo getNetDiskMd5ById(String id) throws Exception {
+        return netDiskMd5Mapper.getById(id);
+    }
+
+    /***
+     * 修改
+     *
+     * @author 王大宸
+     * @date 2023-04-14 23:20:54
+     * @param netDiskMd5Dto netDiskMd5Dto
+     * @return com.github.itdachen.admin.sdk.dto.netDiskMd5
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public NetDiskMd5Dto updateNetDiskMd5(NetDiskMd5Dto netDiskMd5Dto) throws Exception {
+        NetDiskMd5 netDiskMd5 = new NetDiskMd5();
+        BeanUtils.copyProperties(netDiskMd5Dto, netDiskMd5);
+        EntityUtils.setUpdatedInfo(netDiskMd5);
+        netDiskMd5Mapper.updateNetDiskMd5(netDiskMd5);
+        return netDiskMd5Dto;
+    }
+
+    /***
+     * 删除
+     *
+     * @author 王大宸
+     * @date 2023-04-14 23:20:54
+     * @param id 需要删除数据的id
+     * @return int 影响行数
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int removeNetDiskMd5(String id) throws Exception {
+        return netDiskMd5Mapper.deleteNetDiskMd5ById(id);
+    }
+
+}
