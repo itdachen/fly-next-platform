9cef7a61617f223fee2c5285fc844264616ac0b8
王大宸 <240216240@qq.com>
2023-06-27 15:43:36 +0800

    对象转换修改

--- fly-next-admin/src/main/resources/run/run.bat
+++ fly-next-admin/src/main/resources/run/run.bat
@@ -2,31 +2,21 @@ chcp 65001
 @echo off
 
 rem 指定启动文件
-set AppName=fly-next-admin.exe
+set AppName=fly-run.exe
 
 ECHO.
 	ECHO.  [1] install and start
 	ECHO.  [2] stop and uninstall
-	ECHO.  [3] install
-	ECHO.  [4] start
-	ECHO.  [5] restart
-	ECHO.  [6] stop
-	ECHO.  [7] uninstall
-	ECHO.  [8] status
-	ECHO.  [9] EXIT
+	ECHO.  [3] status
+	ECHO.  [4] EXIT
 ECHO.
 
 ECHO.请输入选择项目的序号:
 set /p ID=
 	IF "%id%"=="1" GOTO installandstart
 	IF "%id%"=="2" GOTO stopanduninstall
-	IF "%id%"=="3" GOTO install
-	IF "%id%"=="4" GOTO start
-	IF "%id%"=="5" GOTO restart
-	IF "%id%"=="6" GOTO stop
-	IF "%id%"=="7" GOTO uninstall
-	IF "%id%"=="8" GOTO status
-	IF "%id%"=="9" EXIT
+	IF "%id%"=="3" GOTO status
+	IF "%id%"=="4" EXIT
 PAUSE
 
 rem install
