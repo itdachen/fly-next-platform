b0ea62ea24dbeda8b8677dec5e79a6f9dd1ec03e
王大宸 <240216240@qq.com>
2023-06-30 17:07:04 +0800

    添加生成数据库文档

--- fly-next-admin/src/main/resources/run/run.bat
+++ fly-next-admin/src/main/resources/run/run.bat
@@ -5,64 +5,38 @@ rem 指定启动文件
 set AppName=fly-run.exe
 
 ECHO.
-	ECHO.  [1] install and start
-	ECHO.  [2] stop and uninstall
+	ECHO.  [1] startup
+	ECHO.  [2] shutdown
 	ECHO.  [3] status
 	ECHO.  [4] EXIT
 ECHO.
 
 ECHO.请输入选择项目的序号:
 set /p ID=
-	IF "%id%"=="1" GOTO installandstart
-	IF "%id%"=="2" GOTO stopanduninstall
+	IF "%id%"=="1" GOTO startup
+	IF "%id%"=="2" GOTO shutdown
 	IF "%id%"=="3" GOTO status
 	IF "%id%"=="4" EXIT
 PAUSE
 
-rem install
-:install
-echo install %AppName% ...
-%AppName%  install
-echo install %AppName% success...
-pause
-goto:eof
-
-rem start
-:start
-%AppName%  start
-goto:eof
-
-:restart
-%AppName% restart
-pause
-goto:eof
-
-:stop
-%AppName% stop
-pause
-goto:eof
-
-:uninstall
-%AppName% uninstall
-pause
-goto:eof
-
-:installandstart
+:startup
 %AppName% install
 echo -------------------------------------------
 %AppName% start
+echo startup success...
 pause
 goto:eof
 
-:stopanduninstall
+:shutdown
 %AppName% stop
 echo -------------------------------------------
 %AppName% uninstall
+echo shutdown success...
 pause
 goto:eof
 
 :status
-echo querying %AppName% status ...
+echo querying status ...
 tasklist | find /i "%AppName%"
 pause
 goto:eof
